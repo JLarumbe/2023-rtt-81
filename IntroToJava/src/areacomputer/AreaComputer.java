046ed2a03b45c9e6c42dabd752f6409d66130056
Jorge Larumbe <jorge.larumbem@gmail.com>
2023-09-19 10:11:49 -0400

    Initial commit

---
+++ IntroToJava/src/areacomputer/AreaComputer.java
@@ -0,0 +1,14 @@
+package areacomputer;
+
+public class AreaComputer {
+	public static void main(String[] args) {
+		double radius, area;
+		
+		radius = 20;
+		
+		area = radius * radius * 3.12159;
+		
+		System.out.println("The area for the circle of radius " +
+				 radius + " is " + area);
+	}
+}
