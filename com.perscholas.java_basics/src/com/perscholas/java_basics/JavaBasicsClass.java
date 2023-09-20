827b785cb8cd2980580d7d13586cf8c327cb35ad
Jorge Larumbe <jorge.larumbem@gmail.com>
2023-09-19 20:20:34 -0400

    initial commit of PA 303.2.1

---
+++ com.perscholas.java_basics/src/com/perscholas/java_basics/JavaBasicsClass.java
@@ -0,0 +1,50 @@
+package com.perscholas.java_basics;
+
+public class JavaBasicsClass {
+	public static void main(String[] args) {
+		int x = 1, y = 5;
+
+		int sum = x + y;
+
+		System.out.println("1)Sum: " + sum);
+
+		// ----------------------------------
+
+		double a = 1, b = 3;
+
+		double result = a + b;
+
+		System.out.println("2)Sum: " + result);
+
+		// ----------------------------------
+
+		int num1 = 10;
+		double num2 = 15;
+
+		double total = num1 + num2;
+
+		System.out.println("3)Sum: " + total);
+
+		// ----------------------------------
+
+		int l = 4, m = 2;
+
+		int div = l / m;
+
+		System.out.println("4)Result: " + div);
+
+		// ----------------------------------
+		double o = 4.5, n = 3.5;
+		
+		double div2 = o/n;
+		
+		System.out.println("5)Result: " + (int)div2);
+		
+		// ----------------------------------
+		
+		int s = 5, t = 6;
+		int q = t / s;
+		
+		System.out.println();
+	}
+}
