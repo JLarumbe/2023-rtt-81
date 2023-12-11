c1d9c41305c237a270c6b32070a6ef3123e1e794
Jorge Larumbe <jorge.larumbem@gmail.com>
2023-12-11 14:17:12 -0500

    added time stamp of when a user is created

--- springboot/src/main/java/org/perscholas/springboot/database/entity/User.java
+++ springboot/src/main/java/org/perscholas/springboot/database/entity/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Getter
 @Setter
 @Entity
@@ -19,4 +21,8 @@ public class User {
 
     @Column(name = "password")
     private String password;
+
+    @Column(name = "create_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
 }
--- springboot/src/main/java/org/perscholas/springboot/service/UserService.java
+++ springboot/src/main/java/org/perscholas/springboot/service/UserService.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Slf4j
 @Service
 public class UserService {
@@ -21,13 +23,16 @@ public class UserService {
 
     public User createUser(RegisterUserFormBean form) {
         User user = new User();
-        
+
         user.setEmail(form.getEmail().toLowerCase());
 
         String encoded = passwordEncoder.encode(form.getPassword());
         log.debug("Encoded password: " + encoded);
         user.setPassword(encoded);
+        user.setCreateDate(new Date());
 
         return userDao.save(user);
     }
+
+
 }
